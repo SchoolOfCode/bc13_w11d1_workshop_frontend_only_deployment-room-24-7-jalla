97206a928fd1e03e49941d50ecc4bf80bb251211
Callasaurus <c.sullivandrage@outlook.com>
2022-12-05 14:21:03 +0000

    most original app ever

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
 
+import {useState} from 'react'
+
 function App() {
+  
+  const [count, setCount] = useState(0)
+  
+  function addOneToCount() {
+    setCount(count + 1)
+  }
+
+  function addFiveToCount() {
+    setCount(count + 5)
+  }
+
+  function addTenToCount() {
+    setCount(count + 10)
+  }
+
+  function resetCount() {
+    setCount(0)
+  }
+
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <h1>The most original app ever created</h1>
+      <button className = "buttons" onClick={addOneToCount}> + 1 </button>
+      <button className = "buttons" onClick={addFiveToCount}> + 5 </button>
+      <button className = "buttons" onClick={addTenToCount}> + 10 </button>
+      <h2> {count} </h2>
+      <button className = "buttons" onClick={resetCount}> RESET!!! ⚠️ </button>
     </div>
   );
 }
