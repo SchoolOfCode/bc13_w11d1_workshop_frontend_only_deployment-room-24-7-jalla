25530cde1e00b444a3ba8b0504fdaff07c792dbc
Callasaurus <c.sullivandrage@outlook.com>
2022-12-05 14:39:45 +0000

    orginal app

--- src/App.js
+++ src/App.js
@@ -29,6 +29,12 @@ function App() {
         <li>Jalla</li>
         <li>Callason</li>
       </ul>
+      <h1>The most original app ever created</h1>
+      <button className = "buttons" onClick={addOneToCount}> + 1 </button>
+      <button className = "buttons" onClick={addFiveToCount}> + 5 </button>
+      <button className = "buttons" onClick={addTenToCount}> + 10 </button>
+      <h2> {count} </h2>
+      <button className = "buttons" onClick={resetCount}> RESET!!! ⚠️ </button>
     </div>
   );
 }
